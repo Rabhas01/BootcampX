938dd04141812e52563ef2dd63c73dd1b6403485
Rabhas <rabhas.vashisht@gmail.com>
2022-04-05 22:45:42 +0000

    BootcampX Queries 4

---
+++ migrations/4_queries/7_AVGtime_toStart_assistance.sql
@@ -0,0 +1,2 @@
+SELECT avg(created_at - started_at)
+FROM assistance_requests;
--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -6,9 +6,9 @@ CREATE TABLE teachers (
   end_date DATE
 );
 
-CREATE TABLE assistance_request (
+CREATE TABLE assistance_requests (
   id SERIAL PRIMARY KEY NOT NULL,
-  students_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
   teacher_id INTEGER REFERENCES teachers(id) ON DELETE CASCADE,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   created_at TIMESTAMP,
@@ -16,4 +16,4 @@ CREATE TABLE assistance_request (
   completed_at TIMESTAMP,
   student_feedback TEXT,
   teacher_feedback TEXT
-)
+);
